e2caf86d89ec79f975dc44814820e3dd1d244605
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2023-11-28 10:14:50 -0300

    iniciado tabela utilizando php

---
+++ cadastar.php
@@ -0,0 +1,22 @@
+<?php
+
+/*Variáveis para armazenamento de dados do form*/
+// Em azul é uma variável
+$nome   = $_POST['nome'];
+$email  = $_POST['email'];
+$senha  = $_POST['senha'];
+
+/*Confirma dados infromados pelo usuário */
+// Em senha não iremos colocar a variavel pois não queremos que mostre a senha do usuário
+echo "<h3>Nome: $nome</h3>";
+echo "<h3>Email: $email</h3>";
+echo "<h3>Senha: A senha é secreta</h3>";
+
+// String de inserção no banco
+// Values = Valores
+// Insert Into = Inserir na tabela do phpmydimin
+$cad_usuario = "INSERT INTO usuario(nome, email, senha) VALUES ('$nome','$email','$senha')";
+
+
+
+?>
